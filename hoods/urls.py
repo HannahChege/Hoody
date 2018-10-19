dacce3521e6d4b8123132ebb14e83ab00aeeac2a
HannahChege <chegehannah45@gmail.com>
2018-10-19 15:15:02 +0300

    Added the navbar

--- hoods/urls.py
+++ hoods/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns=[
     url('^$',views.hood,name = 'hood'),
     url(r'^search/', views.search_results, name='search_results'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
 ]
