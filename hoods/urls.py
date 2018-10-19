9c507569f48cb98970084f6e22f24ad48ee2ee31
HannahChege <chegehannah45@gmail.com>
2018-10-19 16:16:17 +0300

    Added the forms

--- hoods/urls.py
+++ hoods/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.hood,name = 'hood'),
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^signup/$', views.signup, name='signup'),
+    url(r'^new/hood$', views.new_hood, name='new-hood'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
 ]
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
