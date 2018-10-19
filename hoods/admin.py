a3d98e402d4c5fdd89907d5ea83bd6238c681c35
HannahChege <chegehannah45@gmail.com>
2018-10-19 15:12:35 +0300

    Adde the admin

--- hoods/admin.py
+++ hoods/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Profile,Business,NeighbourHood
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(NeighbourHood)
