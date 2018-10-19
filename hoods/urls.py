71f964c119456d0460221a2ec66b14270856809c
HannahChege <chegehannah45@gmail.com>
2018-10-19 10:35:04 +0300

    changes

--- hoods/urls.py
+++ hoods/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     url('^$',views.hood,name = 'hood'),
+    url(r'^search/', views.search_results, name='search_results'),
 ]
--- hoods/views.py
+++ hoods/views.py
@@ -6,3 +6,16 @@ from django.shortcuts import render
 # Create your views here.
 def hood(request):
     return render(request,'index.html')
+
+def search_results(request):
+
+    if 'hood' in request.GET and request.GET["hood"]:
+        search_term = request.GET.get("hood")
+        searched_hoods = Hood.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"hoods": searched_hoods})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
