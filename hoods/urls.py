f94b8341fb62dae483fc9db13e48020fe40de353
HannahChege <chegehannah45@gmail.com>
2018-10-22 10:52:33 +0300

    Added the forms

--- hoods/urls.py
+++ hoods/urls.py
@@ -8,9 +8,9 @@ urlpatterns=[
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^new/hood$', views.new_hood, name='new-hood'),
+    url(r'^create/post$', views.create_post, name='create-post'),
     url(r'^profile/(\d+)', views.profile, name='profile'),
     url(r'^newprofile/', views.new_profile, name='new_profile'),
-
     url(r'^business/', views.business, name='business'),
     url(r'^newbusiness/', views.new_business, name='new_business'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
